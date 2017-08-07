7b81f699a5d93946b398df919cdb7bc0437651e5
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-07 11:38:15 +0800

    Fix app.py bug, whitespace in docker-compose-fff.yml

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +7,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
-            
