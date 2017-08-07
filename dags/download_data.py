8663a317896f992bb192e334a419cd6c1d6c86cc
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-07 09:36:16 +0800

    Hack to skip downloading

--- dags/download_data.py
+++ dags/download_data.py
@@ -13,14 +13,15 @@ PLAYER_URL = "https://fantasy.premierleague.com/drf/element-summary/"
 PLAYER_DETAIL_URL = "https://fantasy.premierleague.com/drf/bootstrap-static"
 MAX_PLAYER_ID = 650
 
-
 client = MongoClient(os.environ['MONGO_URL'])
 db = client["fantasy_football"]
 
+
 def download_data(execution_date, **kwargs):
+    if True: return  # hack to skip downloading for now
     response = requests.get(PLAYER_DETAIL_URL)
     data = response.json()
-    
+
     for key, val in data.items():
         collection = db[key]
         if type(val) is not list:
@@ -30,8 +31,8 @@ def download_data(execution_date, **kwargs):
 
     logging.info("got player details")
     collection = db["player_data"]
-    for player_id in range(1, MAX_PLAYER_ID+1):
-        if player_id%100==0:
+    for player_id in range(1, MAX_PLAYER_ID + 1):
+        if player_id % 100 == 0:
             logging.info(player_id)
         response = requests.get("{}/{}".format(PLAYER_URL, player_id))
         data = response.json()
@@ -43,4 +44,5 @@ def download_data(execution_date, **kwargs):
 
 if __name__ == "__main__":
     from datetime import datetime
+
     download_data(datetime.today(), None)
