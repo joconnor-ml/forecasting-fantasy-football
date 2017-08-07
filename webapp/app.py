230762b5bbd4080bd26e8322f0f15250dcb36e49
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-07 11:21:56 +0800

    add simple Flask app

---
+++ webapp/app.py
@@ -0,0 +1,10 @@
+From flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Joe.'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+            
