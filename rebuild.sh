989f10be55788fec8791eb9500f1613387d45003
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-16 15:30:17 +0800

    improve predictions page. Add webapp to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,4 @@
-git pull && docker stop forecastingfantasyfootball_postgres_1 forecastingfantasyfootball_mongo_1 forecastingfantasyfootball_webserver_1 && docker rm forecastingfantasyfootball_postgres_1 forecastingfantasyfootball_mongo_1 forecastingfantasyfootball_webserver_1 && docker-compose -f docker-compose-fff.yml up -d
+git pull && \
+ docker stop forecastingfantasyfootball_postgres_1 forecastingfantasyfootball_mongo_1 forecastingfantasyfootball_webserver_1 forecastingfantasyfootball_webapp_1 && \
+ docker rm forecastingfantasyfootball_postgres_1 forecastingfantasyfootball_mongo_1 forecastingfantasyfootball_webserver_1 forecastingfantasyfootball_webapp_1 && \
+ docker-compose -f docker-compose-fff.yml up -d
