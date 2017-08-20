4eae8e38195908d0d087bca1a8594deecbd4c1e0
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-20 20:45:25 +0800

    remove --verbose

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose-fff.yml build && docker-compose -f docker-compose-fff.yml --verbose up -d
+docker-compose -f docker-compose-fff.yml build && docker-compose -f docker-compose-fff.yml up -d
