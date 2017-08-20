00f490647dd3a1448ee05851bdfdfc66fab04e7e
Joseph O'Connor <joseph.m.oconnor.88@gmail.com>
2017-08-20 18:04:43 +0800

    bugfix

--- dags/build_models.py
+++ dags/build_models.py
@@ -25,14 +25,14 @@ def build_models(execution_date, **kwargs):
             Xtest.index = test_names
             Xtest.to_csv("/data/test_features_gw{}.csv".format(test_week))
         model = model.fit(Xtrain, ytrain)
-        preds[name] = pd.Series(model.predict(Xtest), index=test_names)
+        preds[name] = pd.Series(model.predict(Xtest).values)
         print(name)
         print(preds[name])
         with open("/models/{}_gw{}.pkl".format(name, test_week), "wb") as f:
             pickle.dump(model, f)
     preds = pd.DataFrame(preds)
-    preds["name"] = preds.index
-    preds.to_csv("/preds/gw{}.csv".format(test_week), index=False)
+    preds["name"] = test_names
+    preds.to_csv("/preds/gw{}.csv".format(test_week))
     collection.insert_many(preds.to_dict("records"))
 
 
