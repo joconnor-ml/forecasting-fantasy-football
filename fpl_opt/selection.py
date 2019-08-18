98fba9900317bca14abfaa53e03bcfc4233790b5
joe <joconnor@quant-co.com>
2019-08-18 15:22:59 +0100

    Get rid of outdated data pipelines, add linear programs and notebooks

---
+++ fpl_opt/selection.py
@@ -0,0 +1,68 @@
+import pulp
+import numpy as np
+
+
+def select_team(expected_scores, prices, positions, clubs, total_budget=100, sub_factor=0.2):
+    num_players = len(expected_scores)
+    model = pulp.LpProblem("Constrained value maximisation", pulp.LpMaximize)
+    decisions = [
+        pulp.LpVariable("x{}".format(i), lowBound=0, upBound=1, cat='Integer')
+        for i in range(num_players)
+    ]
+    captain_decisions = [
+        pulp.LpVariable("y{}".format(i), lowBound=0, upBound=1, cat='Integer')
+        for i in range(num_players)
+    ]
+    sub_decisions = [
+        pulp.LpVariable("z{}".format(i), lowBound=0, upBound=1, cat='Integer')
+        for i in range(num_players)
+    ]
+
+    # objective function:
+    model += sum((captain_decisions[i] + decisions[i] + sub_decisions[i] * sub_factor) * expected_scores[i]
+                 for i in range(num_players)), "Objective"
+
+    # cost constraint
+    model += sum(
+        (decisions[i] + sub_decisions[i]) * prices[i] for i in range(num_players)) <= total_budget  # total cost
+
+    # position constraints
+    # 1 starting goalkeeper
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 1) == 1
+    # 2 total goalkeepers
+    model += sum(decisions[i] + sub_decisions[i] for i in range(num_players) if positions[i] == 1) == 2
+
+    # 3-5 starting defenders
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 2) >= 3
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 2) <= 5
+    # 5 total defenders
+    model += sum(decisions[i] + sub_decisions[i] for i in range(num_players) if positions[i] == 2) == 5
+
+    # 3-5 starting midfielders
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 3) >= 3
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 3) <= 5
+    # 5 total midfielders
+    model += sum(decisions[i] + sub_decisions[i] for i in range(num_players) if positions[i] == 3) == 5
+
+    # 1-3 starting attackers
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 4) >= 1
+    model += sum(decisions[i] for i in range(num_players) if positions[i] == 4) <= 3
+    # 3 total attackers
+    model += sum(decisions[i] + sub_decisions[i] for i in range(num_players) if positions[i] == 4) == 3
+
+    # club constraint
+    for club_id in np.unique(clubs):
+        model += sum(
+            decisions[i] + sub_decisions[i] for i in range(num_players) if clubs[i] == club_id) <= 3  # max 3 players
+
+    model += sum(decisions) == 11  # total team size
+    model += sum(captain_decisions) == 1  # 1 captain
+
+    for i in range(num_players):
+        model += (decisions[i] - captain_decisions[i]) >= 0  # captain must also be on team
+        model += (decisions[i] + sub_decisions[i]) <= 1  # subs must not be on team
+
+    model.solve()
+    print("Total expected score = {}".format(model.objective.value()))
+
+    return decisions, captain_decisions, sub_decisions
---
+++ fpl_opt/transfers.py
@@ -0,0 +1,108 @@
+import pulp
+import numpy as np
+
+position_data = {
+    "gk": {"position_id": 1, "min_starters": 1, "max_starters": 1, "num_total": 2},
+    "df": {"position_id": 2, "min_starters": 3, "max_starters": 5, "num_total": 5},
+    "mf": {"position_id": 3, "min_starters": 3, "max_starters": 5, "num_total": 5},
+    "fw": {"position_id": 4, "min_starters": 1, "max_starters": 3, "num_total": 3},
+}
+
+
+def get_decision_array(name, length):
+    return [
+        pulp.LpVariable("{}_{}".format(name, i), lowBound=0, upBound=1, cat='Integer')
+        for i in range(length)
+    ]
+
+
+class TransferOptimiser:
+    def __init__(self, expected_scores, buy_prices, sell_prices, positions, clubs):
+        self.expected_scores = expected_scores
+        self.buy_prices = buy_prices
+        self.sell_prices = sell_prices
+        self.positions = positions
+        self.clubs = clubs
+        self.num_players = len(expected_scores)
+
+    def instantiate_decision_arrays(self):
+        # we will make transfers in and out of the squad, and then pick subs and captains from that squad
+        transfer_in_decisions_free = get_decision_array("transfer_in_free", self.num_players)
+        transfer_in_decisions_paid = get_decision_array("transfer_in_paid", self.num_players)
+        transfer_out_decisions = get_decision_array("transfer_out_paid", self.num_players)
+        # total transfers in will be useful later
+        transfer_in_decisions = [a + b for a, b in zip(transfer_in_decisions_free, transfer_in_decisions_paid)]
+
+        sub_decisions = get_decision_array("subs", self.num_players)
+        captain_decisions = get_decision_array("captain", self.num_players)
+        return transfer_in_decisions_free, transfer_in_decisions_paid, transfer_out_decisions, transfer_in_decisions, sub_decisions, captain_decisions
+
+    def encode_player_indices(self, indices):
+        decisions = np.zeros(self.num_players)
+        decisions[indices] = 1
+        return decisions
+
+    def apply_transfer_constraints(self, model, transfer_in_decisions_free, transfer_in_decisions,
+                                   transfer_out_decisions, budget_now):
+        # only 1 free transfer
+        model += sum(transfer_in_decisions_free) <= 1
+
+        # budget constraint
+        transfer_in_cost = sum(x * price for x, price in zip(transfer_in_decisions, self.buy_prices))
+        transfer_out_cost = sum(x * price for x, price in zip(transfer_out_decisions, self.sell_prices))
+        budget_next_week = budget_now + transfer_out_cost - transfer_in_cost
+        model += budget_next_week >= 0
+
+
+    def solve(self, current_squad_indices, budget_now, sub_factor):
+        current_squad_decisions = self.encode_player_indices(current_squad_indices)
+
+        model = pulp.LpProblem("Transfer optimisation", pulp.LpMaximize)
+        transfer_in_decisions_free, transfer_in_decisions_paid, transfer_out_decisions, transfer_in_decisions, sub_decisions, captain_decisions = self.instantiate_decision_arrays()
+
+        # calculate new team from current team + transfers
+        next_week_squad = [
+            current_squad_decisions[i] + transfer_in_decisions[i] - transfer_out_decisions[i]
+            for i in range(self.num_players)
+        ]
+        starters = [a - b for a, b in zip(next_week_squad, sub_decisions)]
+
+        # points penalty for additional transfers
+        transfer_penalty = sum(transfer_in_decisions_paid) * 4
+
+        self.apply_transfer_constraints(model, transfer_in_decisions_free, transfer_in_decisions,
+                                        transfer_out_decisions, budget_now)
+        self.apply_formation_constraints(model, squad=next_week_squad, starters=starters,
+                                         subs=sub_decisions, captains=captain_decisions)
+
+        # objective function:
+        model += self.get_objective(starters, sub_decisions, captain_decisions, sub_factor, transfer_penalty), "Objective"
+        model.solve()
+
+        return transfer_in_decisions, transfer_out_decisions, starters, sub_decisions, captain_decisions
+
+    def get_objective(self, starters, subs, captains, sub_factor, transfer_penalty):
+        starter_points = sum(x * score for x, score in zip(starters, self.expected_scores))
+        sub_points = sum(x * score for x, score in zip(subs, self.expected_scores)) * sub_factor
+        captain_points = sum(x * score for x, score in zip(captains, self.expected_scores))
+        return starter_points + sub_points + captain_points - transfer_penalty
+
+    def apply_formation_constraints(self, model, squad, starters, subs, captains):
+        for position, data in position_data.items():
+            # formation constraints
+            model += sum(starter for starter, position in zip(starters, self.positions) if position == data["position_id"]) >= data["min_starters"]
+            model += sum(starter for starter, position in zip(starters, self.positions) if position == data["position_id"]) <= data["max_starters"]
+            model += sum(selected for selected, position in zip(squad, self.positions) if position == data["position_id"]) == data["num_total"]
+
+        # club constraint
+        for club_id in np.unique(self.clubs):
+            model += sum(selected for selected, club in zip(squad, self.clubs) if club == club_id) <= 3  # max 3 players
+
+        # total team size
+        model += sum(starters) == 11
+        model += sum(squad) == 15
+        model += sum(captains) == 1
+
+        for i in range(self.num_players):
+            model += (starters[i] - captains[i]) >= 0  # captain must also be on team
+            model += (starters[i] + subs[i]) <= 1  # subs must not be on team
