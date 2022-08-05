ea579c5c433ae70e1054740768aa3192ee44c607
Joe <joe@transitionzero.org>
2022-08-05 15:35:48 +0100

    Use `code` for player ID across seasons

--- fpl_forecast/utils.py
+++ fpl_forecast/utils.py
@@ -1,5 +1,5 @@
 def get_new_players(df, last_year_df):
-    return df[~df["player_indices"].isin(last_year_df["player_indices"])].index()
+    return df[~df["code"].isin(last_year_df["code"])].index()
 
 def get_transferred_players(df, last_year_df):
     pass
