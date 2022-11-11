639d5e4658dba414c47c04de735cf95756e6f91d
Joe <joe@transitionzero.org>
2022-11-11 22:14:44 +0000

    read individual gw files

--- fpl_forecast/utils.py
+++ fpl_forecast/utils.py
@@ -1,7 +1,46 @@
 import pandas as pd
+from loguru import logger
 
 PLAYER_ID_COL = "code"
 SEASONS = ["2019-20", "2020-21", "2021-22", "2022-23"]
+GW_COLS = [
+    "name",
+    "position",
+    "team",
+    "xP",
+    "assists",
+    "bonus",
+    "bps",
+    "clean_sheets",
+    "creativity",
+    "element",
+    "fixture",
+    "goals_conceded",
+    "goals_scored",
+    "ict_index",
+    "influence",
+    "kickoff_time",
+    "minutes",
+    "opponent_team",
+    "own_goals",
+    "penalties_missed",
+    "penalties_saved",
+    "red_cards",
+    "round",
+    "saves",
+    "selected",
+    "team_a_score",
+    "team_h_score",
+    "threat",
+    "total_points",
+    "transfers_balance",
+    "transfers_in",
+    "transfers_out",
+    "value",
+    "was_home",
+    "yellow_cards",
+    "GW",
+]
 
 
 def generate_targets(df, horizon, target_cols=("total_points", "minutes")):
@@ -28,14 +67,26 @@ def generate_rolling_features(df, cols, windows=(3, 10, 19), aggs=("mean", "medi
     return pd.concat(feats, axis=1).reset_index(PLAYER_ID_COL, drop=True).sort_index()
 
 
+def get_gw_data(season):
+    dfs = []
+    for i in range(1, 39):
+        try:
+            df = pd.read_csv(
+                f"https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season}/gws/gw{i}.csv",
+                usecols=GW_COLS,
+            )
+            dfs.append(df)
+        except Exception as e:
+            logger.exception(e)
+            break
+    return pd.concat(dfs).rename(columns={"team": "team_name"})
+
+
 def get_player_data(seasons):
     df = (
         pd.concat(
             [
-                pd.read_csv(
-                    f"https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season}/gws/merged_gw.csv"
-                )
-                .rename(columns={"team": "team_name"})
+                get_gw_data(season)
                 .merge(
                     pd.read_csv(
                         f"https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/{season}/players_raw.csv",
