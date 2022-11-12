c166e5784a12d4dd0e40fc1340da1b12305b9a1a
Joe <joe@transitionzero.org>
2022-11-12 11:31:39 +0000

    Test authentication

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-POINTS_DATA_PATH = r"C:\Users\Joe\Downloads\points (1)\points.csv"
-PLAYING_DATA_PATH = r"C:\Users\Joe\Downloads\playing (1)\playing.csv"
+POINTS_DATA_PATH = r"C:\Users\Joe\Downloads\points (3)\points.csv"
+PLAYING_DATA_PATH = r"C:\Users\Joe\Downloads\playing (2)\playing.csv"
 MAX_HORIZONS = 4
+
 
 @st.cache
 def get_points_data():
     df = pd.read_csv(POINTS_DATA_PATH)
     return df
+
 
 @st.cache
 def get_playing_data():
@@ -16,21 +18,23 @@ def get_playing_data():
     return df
 
 
-def main():
+@st.cache
+def get_forecast_data():
     playing = get_playing_data()
     points = get_points_data()[["name", "team", "score_pred", "horizon"]]
-    df = playing.merge(points, how="left", on=["name", "team", "horizon"]).set_index("name")[["horizon", "score_pred", "playing_chance"]]
+    df = playing.merge(points, how="left", on=["name", "team", "horizon"]).set_index(
+        "name"
+    )[["horizon", "score_pred", "playing_chance"]]
     df = df.rename(columns={"score_pred": "score_if_playing"})
     df["score_pred"] = df["score_if_playing"] * df["playing_chance"]
+    return df
 
-    players = st.multiselect(
-        "Choose players", list(df.index.unique())
-    )
+def main():
+    df = get_forecast_data()
+    players = st.multiselect("Choose players", list(df.index.unique()))
     if not players:
         st.error("Please select at least one player.")
-    horizons = st.multiselect(
-        "Choose horizons", range(1, MAX_HORIZONS+1)
-    )
+    horizons = st.multiselect("Choose horizons", range(1, MAX_HORIZONS + 1))
     if not horizons:
         st.error("Please select at least one horizon.")
     else:
