f2784ed7003a83ccbdc2e58e374d883e6e9d3674
Joe <joe@transitionzero.org>
2022-11-15 11:56:51 +0000

    generate data for test

--- app/app.py
+++ app/app.py
@@ -7,21 +7,21 @@ MAX_HORIZONS = 4
 
 
 @st.cache
-def get_points_data():
-    df = pd.read_csv(POINTS_DATA_PATH)
+def get_points_data(path):
+    df = pd.read_csv(path)
     return df
 
 
 @st.cache
-def get_playing_data():
-    df = pd.read_csv(PLAYING_DATA_PATH)
+def get_playing_data(path):
+    df = pd.read_csv(path)
     return df
 
 
 @st.cache
-def get_forecast_data():
-    playing = get_playing_data()
-    points = get_points_data()[["name", "team", "score_pred", "horizon"]]
+def get_forecast_data(points_path, playing_path):
+    playing = get_playing_data(playing_path)
+    points = get_points_data(points_path)[["name", "team", "score_pred", "horizon"]]
     df = playing.merge(points, how="left", on=["name", "team", "horizon"]).set_index(
         "name"
     )[["horizon", "score_pred", "playing_chance"]]
@@ -31,7 +31,7 @@ def get_forecast_data():
 
 
 def main():
-    df = get_forecast_data()
+    df = get_forecast_data(POINTS_DATA_PATH, PLAYING_DATA_PATH)
     players = st.multiselect("Choose players", list(df.index.unique()))
     if not players:
         st.error("Please select at least one player.")
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,13 +3,13 @@ from app.app import get_points_data, get_playing_data, get_forecast_data
 
 class TestApp:
     def test_get_points_data(self):
-        df = get_points_data()
+        df = get_points_data("points.csv")
         assert not df.empty
 
     def test_get_playing_data(self):
-        df = get_playing_data()
+        df = get_playing_data("playing.csv")
         assert not df.empty
 
     def test_get_forecast_data(self):
-        df = get_forecast_data()
+        df = get_forecast_data("points.csv", "playing.csv")
         assert not df.empty
