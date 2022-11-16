575776f263a806fb70899ba91deecae94728f724
Joe <joe@transitionzero.org>
2022-11-16 21:18:45 +0000

    downgrade pandas

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,13 +3,13 @@ from app.app import get_points_data, get_playing_data, get_forecast_data
 
 class TestApp:
     def test_get_points_data(self):
-        df = get_points_data("points.pq")
+        df = get_points_data("points.csv")
         assert not df.empty
 
     def test_get_playing_data(self):
-        df = get_playing_data("playing.pq")
+        df = get_playing_data("playing.csv")
         assert not df.empty
 
     def test_get_forecast_data(self):
-        df = get_forecast_data("points.pq", "playing.pq")
+        df = get_forecast_data("points.csv", "playing.csv")
         assert not df.empty
