88f54ea256a696a2bbd9dd7226b8090b582c4b7b
Joe <joe@transitionzero.org>
2022-12-19 14:44:03 +0000

    Write features to CSV

--- scripts/train_all_points.py
+++ scripts/train_all_points.py
@@ -5,7 +5,7 @@ import pandas as pd
 from . import train_points_model
 
 
-def main(max_horizon, output_path, scores_path):
+def main(max_horizon, output_path, scores_path, features_path):
     all_preds = []
     all_scores = []
     all_features = []
@@ -20,6 +20,7 @@ def main(max_horizon, output_path, scores_path):
             all_features.append(test_features)
     pd.concat(all_preds).to_csv(output_path)
     pd.concat(all_scores).to_csv(scores_path)
+    pd.concat(all_features).to_csv(features_path)
 
 
 if __name__ == "__main__":
@@ -28,4 +29,4 @@ if __name__ == "__main__":
     parser.add_argument("--outfile", type=str, required=True)
     parser.add_argument("--score_path", type=str, required=True)
     args = parser.parse_args()
-    main(args.horizon, args.outfile, args.score_path)
+    main(args.horizon, args.outfile, args.score_path, args.features_path)
