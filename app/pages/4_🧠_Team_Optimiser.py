9ca75230d457797e88dac0ecfff35e9c340c8f45
Joe <joe@transitionzero.org>
2022-12-26 16:27:49 +0000

    lint

--- app/pages/4_🧠_Team_Optimiser.py
+++ app/pages/4_🧠_Team_Optimiser.py
@@ -7,7 +7,12 @@ settings = utils.get_settings()
 
 def main():
     budget = float(st.text_input("Budget", "100"))
-    selection_df = utils.pick_team(settings.points_data_path, settings.playing_data_path, settings.bucket_name, budget)
+    selection_df = utils.pick_team(
+        settings.points_data_path,
+        settings.playing_data_path,
+        settings.bucket_name,
+        budget,
+    )
     st.dataframe(
         selection_df,
         use_container_width=True,
