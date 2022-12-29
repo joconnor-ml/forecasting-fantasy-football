Fix weighting

fpl_forecast/total_points.py
@@ -107,11 +107,12 @@ class PointsModel:
         return np.clip(targets, 0, np.inf) ** 0.66
 
     def train(self, train_features, train_targets, weights, **fit_kwargs):
-        model_name = self.model.steps[-1][-1].__name__
-        model_args = {f"{model_name}__sample_weight": weights}
         self.feature_names = train_features.columns
         self.model = self.model.fit(
-            train_features, self.transform(train_targets), **model_args, **fit_kwargs
+            train_features,
+            self.transform(train_targets),
+            model__sample_weight=weights,
+            **fit_kwargs,
         )
         return self
 
@@ -127,18 +128,18 @@ class PointsModel:
     def generate_features(self, df):
         return pd.concat(
             [
-                utils.generate_targets(
-                    df, self.horizon, ["win_prob"]
+                utils.generate_targets(df, self.horizon, ["win_prob"]),
+                self.transform(
+                    utils.generate_rolling_features(
+                        df,
+                        ["xP"],
+                        aggs=("mean",),
+                    )
                 ),
-                self.transform(utils.generate_rolling_features(
-                    df,
-                    ["xP"],
-                    aggs=("mean",),
-                )),
-                #(
+                # (
                 #    utils.generate_lag_features(df, ["value_rank"], lags=(0,))
                 #    + np.random.uniform(0, 1)
-                #).clip(1, 3),
+                # ).clip(1, 3),
             ],
             axis=1,
         )
@@ -151,13 +152,11 @@ class GKModel(PointsModel):
     def generate_features(self, df):
         return pd.concat(
             [
-                utils.generate_targets(
-                    df, self.horizon, ["win_prob"]
-                ),
+                utils.generate_targets(df, self.horizon, ["win_prob"]),
                 utils.generate_rolling_features(
                     df, ["saves", "minutes"], aggs=("mean",)
                 ),
-                #utils.generate_lag_features(df, ["value_rank"], lags=(0,)),
+                # utils.generate_lag_features(df, ["value_rank"], lags=(0,)),
             ],
             axis=1,
         )

scripts/train_points_model.py
@@ -42,7 +42,11 @@ def main(position: str, horizon: int):
         ## benchmark:
         benchmark_pred = np.ones_like(val_targets) * val_targets.mean()
         benchmark_scores = model.get_scores(val_targets, benchmark_pred)
-        model = model.train(train_features, train_targets, weights=df[train_filter]["selected_by_percent"]**0.5)
+        model = model.train(
+            train_features,
+            train_targets,
+            weights=df[train_filter]["selected_by_percent"] ** 0.5,
+        )
         preds = model.predict(val_features)
         top_preds = model.predict(top_val_features)
         scores = model.get_scores(val_targets, preds)